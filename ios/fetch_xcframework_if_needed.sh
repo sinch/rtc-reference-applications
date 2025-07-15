bc3f9a2027e13571415dbee52332d14011208f21
CI Pipeline <dev@sinch.com>
2025-07-15 14:53:07 +0200

    Auto update iOS SDK to 5.33.16%2b9300dfe7

--- ios/fetch_xcframework_if_needed.sh
+++ ios/fetch_xcframework_if_needed.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-readonly SDK_VERSION="5.31.26%2bbb3c5c00"
+readonly SDK_VERSION="5.33.16%2b9300dfe7"
 
 readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 readonly SDK_SHORT_VERSION=$(echo $SDK_VERSION | cut -d% -f1)
